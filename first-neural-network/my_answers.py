54f9a98df4de91b0d3b682014b04e06c0071a1d1
Alexander Gorskih <alexander.gorskih@gmail.com>
2018-01-19 08:22:09 +0300

    checkpoint

--- first-neural-network/my_answers.py
+++ first-neural-network/my_answers.py
@@ -106,8 +106,8 @@ class NeuralNetwork(object):
             n_records: number of records
 
         '''
-        self.weights_hidden_to_output += self.lr * delta_weights_h_o # update hidden-to-output weights with gradient descent step
-        self.weights_input_to_hidden += self.lr * delta_weights_i_h # update input-to-hidden weights with gradient descent step
+        self.weights_hidden_to_output += self.lr * delta_weights_h_o / n_records # update hidden-to-output weights with gradient descent step
+        self.weights_input_to_hidden += self.lr * delta_weights_i_h / n_records # update input-to-hidden weights with gradient descent step
 
     def run(self, features):
         ''' Run a forward pass through the network with input features 
@@ -132,7 +132,7 @@ class NeuralNetwork(object):
 #########################################################
 # Set your hyperparameters here
 ##########################################################
-iterations = 500
-learning_rate = 0.01
-hidden_nodes = 5
+iterations = 5000
+learning_rate = 0.5
+hidden_nodes = 8
 output_nodes = 1
